dea132fce60fbbdd6234650354b5b2c70cf6d9a4
suba12-m <m.subasree2002@gmail.com>
2024-08-28 20:15:33 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MeaningFinder from './MeaningFinder';
+
+function App() {
+  return (
+    <div className="App">
+      <MeaningFinder />
+    </div>
+  );
+}
+
+export default App;
+
